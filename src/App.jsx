757566849a3bef677dabf1e2c35e79f35cd49988
Florian Lemaire <2vsk22@gmail.com>
2024-06-09 13:10:01 +0200

    Commencement du projet Kasa

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+
+function App() {
+  return <Banner title="Bonjour" />;
+}
+
+export default App;
---
+++ src/components/Banner/Banner.style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BannerText = styled.div`
+  color: blue;
+  background-color: black;
+`;
+
+export const BannerTextAlt = styled.div`
+  color: red;
+  background-color: black;
+`;
